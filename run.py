e5bdef591df6b9379d43dafd49612ed211585f94
Lucas Mendes <lucas.mendes@4linux.com.br>
2023-04-11 21:02:26 -0300

    ajustando host

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from app import app
 import os
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True, port=os.getenv("PORT", default=5000), host=os.getenv("HOST", default="0.0.0.0"))
